8f7e2369b79d79dbcaef52713c41783d41703d07
afoisl <ruaql0810@gmail.com>
2024-11-03 11:17:32 +0900

    Validation

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -4,6 +4,7 @@ import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -31,7 +32,7 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) { // @ModelAttribute 생략
+    public String signup(@Valid SignupRequestDto requestDto) { // @ModelAttribute 생략
         userService.signup(requestDto);
         return "redirect:/api/user/login-page";
     }
